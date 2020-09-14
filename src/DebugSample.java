63b1b23fb22e8d36f6a810fd089b0a86d15d5b90
wrighta <wrighta@iadt.ie>
2020-09-14 21:49:27 +0100

    Initial commit

---
+++ src/DebugSample.java
@@ -0,0 +1,5 @@
+public class DebugSample {
+    public static void main(String args[]){
+        System.out.println("Hello");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 1022;
+
+        int y = 0;
+
+        System.out.println("The result of x divided by y is " + x/y);
+
+        System.out.println("This is the end of the program, did I get here?");
+    }
+}
